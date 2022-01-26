2c63ae12b59a9ebac06021ae5b46a072fd054a17
geekypankaj998 <pankaj11111998@gmail.com>
2022-01-27 02:54:35 +0530

    Setting up Directory and Express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+app.listen(port,function(err){
+  if(err){
+      console.log(`Error occured at ${port} and the error is : ${err}`);
+  }
+  console.log(`Server is up and running at port : ${port}`); 
+});
